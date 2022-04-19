dc1a347b7d8c61605795f3156f41e9a3792dcb70
Victor <100092662+victor0x16@users.noreply.github.com>
2022-04-19 18:55:33 -0300

    add code component

---
+++ src/components/Code.jsx
@@ -0,0 +1,9 @@
+export default function Code() {
+    return (
+        <code>
+           { ```var component = function () \{
+                return <h1>Header</h1>
+            }```}
+        </code>
+    )
+} 
